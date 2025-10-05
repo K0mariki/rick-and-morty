71ac9219c7e0d2716cf2f0987007ca08f00922c6
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-06 02:13:09 +0300

    Исправлен импорт layout-компонента Header

--- src/layouts/Layout/Layout.jsx
+++ src/layouts/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer";
-import Header from "../Header copy/Header";
+import Header from "../Header";
 
 export default function Layout() {
   return (
