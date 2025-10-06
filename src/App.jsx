8a95a2aea6953d5a2a948730c6048cbf4923f390
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-06 15:59:11 +0300

    Добавлен роутинг между страницами

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles/index";
+import Layout from "./layouts/Layout";
+import Characters from "./pages/Characters";
+import CharacterDetails from "./pages/CharacterDetails";
+import Episodes from "./pages/Episodes";
+import EpisodeDetails from "./pages/EpisodeDetails";
+import Locations from "./pages/Locations";
+import LocationDetails from "./pages/LocationDetails";
 
 function App() {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, element: <Characters /> },
+        { path: "character/:characterId", element: <CharacterDetails /> },
+        { path: "episodes", element: <Episodes /> },
+        { path: "episode/:episodeId", element: <EpisodeDetails /> },
+        { path: "location", element: <Locations /> },
+        { path: "location/:locationId", element: <LocationDetails /> },
+        { path: "*", element: <Characters /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
