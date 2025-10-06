33eeb9ef311d47c5381dc5ad0528c90ec3502cfa
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-07 01:07:17 +0300

    Добавлен props 'className'

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
-import "./Logo.scss"
+import "./Logo.scss";
 
-export default function Logo() {
+export default function Logo(props) {
+  const { className } = props;
   return (
     <Link
-      className="logo"
+      className={`logo ${className}`}
       to={"characters"}
       title="Characters"
       aria-label="Characters"
