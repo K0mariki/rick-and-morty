e554c802f1e5545ae4a4d46386434fdb7953db6e
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-06 23:35:22 +0300

    Добавлены css свойства

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,5 +1,11 @@
 import "./Footer.scss";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="footer__inner">
+        <p className="footer__text">Make with ❤️ for the MobProgramming team</p>
+      </div>
+    </footer>
+  );
 }
