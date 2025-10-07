21feccc1f67a2d0d6d6764c5d11bfe29a232150c
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-07 19:57:19 +0300

    Создан компонент, добавлена разметка и стили

---
+++ src/sections/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import "./Hero.scss";
+
+export default function Hero(props) {
+  const { children, image } = props;
+
+  return (
+    <section className="hero">
+      <div className="hero__inner container">
+        {image && <img src={`src/assets/images/${image}`} className="hero__image" alt={image}/>}
+        <div className="hero__grid">{children}</div>
+      </div>
+    </section>
+  );
+}
