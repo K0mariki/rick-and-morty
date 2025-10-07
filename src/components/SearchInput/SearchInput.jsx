0e9411a732392993cef0ed3224d726f3d20032e1
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-07 19:56:27 +0300

    Создан компонент, добавлена разметка и стили

---
+++ src/components/SearchInput/SearchInput.jsx
@@ -0,0 +1,15 @@
+import "./SearchInput.scss";
+import Icon from "../Icon";
+
+export default function SearchInput() {
+  return (
+      <label className="search">
+        <Icon className="search__icon" name="search.svg"/>
+        <input
+          type="text"
+          className="search__input"
+          placeholder={"Filter by name or episode (ex. S01 or S01E02)"}
+        />
+      </label>
+  );
+}
