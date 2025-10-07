e3932c9e1441aad840398735423b4a3df6912c11
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-08 02:29:46 +0300

    Создана страница NotFound.jsx

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <div className="container" style={{ textAlign: "center" }}>
+        <h1>404</h1>
+      </div>
+    </div>
+  );
+}
