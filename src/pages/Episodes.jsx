44f897cf7c1f514b297bff2c68dd4aab67fc8ed3
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-08 02:31:22 +0300

    Добавлена разметка страницы

--- src/pages/Episodes.jsx
+++ src/pages/Episodes.jsx
@@ -1,3 +1,29 @@
+import { useLoaderData } from "react-router-dom";
+import SearchInput from "../components/SearchInput";
+import Hero from "../sections/Hero";
+import BaseCard from "../components/BaseCard";
+import Section from "../sections/Section";
+
 export default function Episodes() {
-    return <>Episodes</>
+  const episodes = useLoaderData();
+  return (
+    <>
+      <Hero image="rick-and-morty.png">
+        <SearchInput></SearchInput>
+      </Hero>
+      <Section title="All episodes on Rick and Morty" hiddenTitle>
+        <ul className="section__cards-list">
+          {episodes.results.map((episode) => (
+            <li key={episode.id} className="section__cards-list-item">
+              <BaseCard
+                title={episode.name}
+                date={episode.air_date}
+                episode={episode.episode}
+              />
+            </li>
+          ))}
+        </ul>
+      </Section>
+    </>
+  );
 }
--- src/pages/Locations.jsx
+++ src/pages/Locations.jsx
@@ -1,3 +1,35 @@
+import { useLoaderData } from "react-router-dom";
+import SearchInput from "../components/SearchInput";
+import SelectsMenu from "../components/SelectsMenu";
+import Select from "../components/Select";
+import Hero from "../sections/Hero";
+import Section from "../sections/Section";
+import BaseCard from "../components/BaseCard";
+
 export default function Locations() {
-    return <>Locations</>
+  const locations = useLoaderData();
+
+  return (
+    <>
+      <Hero image="rick-and-morty-portal.png">
+        <SearchInput></SearchInput>
+        <SelectsMenu>
+          <Select defaultValue="Type"></Select>
+          <Select defaultValue="Dimension"></Select>
+        </SelectsMenu>
+      </Hero>
+      <Section title="All episodes on Rick and Morty" hiddenTitle>
+        <ul className="section__cards-list">
+          {locations.results.map((location) => (
+            <li key={location.id} className="section__cards-list-item">
+              <BaseCard
+                title={location.name}
+                type={location.type}
+              />
+            </li>
+          ))}
+        </ul>
+      </Section>
+    </>
+  );
 }
