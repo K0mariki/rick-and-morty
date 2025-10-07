ae9a2404ea1711f6ece798d9f9d4602eaa42a885
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-08 02:29:08 +0300

    Добавлена загрузка

--- src/layouts/Layout/Layout.jsx
+++ src/layouts/Layout/Layout.jsx
@@ -1,13 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../Footer";
 import Header from "../Header";
 
 export default function Layout() {
+  const navigation = useNavigation();
+  
   return (
     <>
       <Header />
       <main>
-        <Outlet />
+        {navigation.state === "loading" ? (
+          <div className="container" style={{display:"flex", alignItems:"center", flexDirection:"column"}}>
+            <img src="src\assets\images\loading.png"></img>
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <Outlet />
+        )}
       </main>
       <Footer />
     </>
