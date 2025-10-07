5a32cd336db6206982da6283141d537a50444b61
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-08 02:38:13 +0300

    Добавлена загрузка данных до рендара страницы и отлов ошибок

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import "./styles/index";
 import Layout from "./layouts/Layout";
 import Characters from "./pages/Characters";
@@ -7,6 +11,11 @@ import Episodes from "./pages/Episodes";
 import EpisodeDetails from "./pages/EpisodeDetails";
 import Locations from "./pages/Locations";
 import LocationDetails from "./pages/LocationDetails";
+import NotFound from "./pages/NotFound";
+import fetchCharacters from "./api/fetchCharacters";
+import fetchEpisodes from "./api/fetchEpisodes";
+import fetchLocation from "./api/fetchLocation";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   const router = createBrowserRouter([
@@ -14,13 +23,38 @@ function App() {
       path: "/",
       element: <Layout />,
       children: [
-        { index: true, element: <Characters /> },
-        { path: "character/:characterId", element: <CharacterDetails /> },
-        { path: "episodes", element: <Episodes /> },
-        { path: "episode/:episodeId", element: <EpisodeDetails /> },
-        { path: "locations", element: <Locations /> },
-        { path: "location/:locationId", element: <LocationDetails /> },
-        { path: "*", element: <Characters /> },
+        { index: true, element: <Navigate to="/characters" /> },
+        {
+          path: "characters",
+          element: <Characters />,
+          loader: fetchCharacters,
+          errorElement: <ErrorBoundary />,
+        },
+        {
+          path: "character/:characterId",
+          element: <CharacterDetails />,
+        },
+        {
+          path: "episodes",
+          element: <Episodes />,
+          loader: fetchEpisodes,
+          errorElement: <ErrorBoundary />,
+        },
+        {
+          path: "episode/:episodeId",
+          element: <EpisodeDetails />,
+        },
+        {
+          path: "locations",
+          element: <Locations />,
+          loader: fetchLocation,
+          errorElement: <ErrorBoundary />,
+        },
+        {
+          path: "location/:locationId",
+          element: <LocationDetails />,
+        },
+        { path: "*", element: <NotFound /> },
       ],
     },
   ]);
