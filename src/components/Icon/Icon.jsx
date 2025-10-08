91957f29de29a2f209affd85ea893cfed94531b4
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-08 03:15:08 +0300

    Добавлен iconMap

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,4 +1,22 @@
+import search from "./../../assets/icons/search.svg";
+import arrowBack from "./../../assets/icons/arrow-back.svg";
+import arrowDropDown from "./../../assets/icons/arrow-drop-down.svg";
+import arrowRight from "./../../assets/icons/arrow-right.svg";
+import close from "./../../assets/icons/close.svg";
+import filter from "./../../assets/icons/filter.svg";
+
+const iconMap = {
+  search: search,
+  arrowBack: arrowBack,
+  arrowDropDown: arrowDropDown,
+  arrowRight: arrowRight,
+  close: close,
+  filter: filter
+}
+
 export default function Icon(props) {
   const { name = "filter", className } = props;
-  return <img src={`src/assets/icons/${name}`} alt={name} className={className}></img>;
+  const iconSrc = iconMap[name];
+
+  return <img src={iconSrc} alt={name} className={className}></img>;
 }
