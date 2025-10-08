58d256d3e4b779780184a87e27cdbc2144e6315a
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-08 03:15:31 +0300

    Добавлен imageHeroMap

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,12 +1,22 @@
 import "./Hero.scss";
+import rickAndMortyLogo from "./../../assets/images/rick-and-morty-logo.svg";
+import rickAndMortyPortal from "./../../assets/images/rick-and-morty-portal.png";
+import rickAndMorty from "./../../assets/images/rick-and-morty.png";
+
+const imgHeroMap = {
+  rickAndMortyLogo,
+  rickAndMortyPortal,
+  rickAndMorty,
+};
 
 export default function Hero(props) {
   const { children, image } = props;
+  const imageSrc = imgHeroMap[image];
 
   return (
     <section className="hero">
       <div className="hero__inner container">
-        {image && <img src={`./../src/assets/images/${image}`} className="hero__image" alt={image}/>}
+        {image && <img src={imageSrc} className="hero__image" alt={image} />}
         <div className="hero__grid">{children}</div>
       </div>
     </section>
