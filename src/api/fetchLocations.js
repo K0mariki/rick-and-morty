db5c6375c8218050123b37612c369fdfa7c28dfc
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-08 20:10:10 +0300

    Изменено имя файла fetchLocation -> fetchLocations

---
+++ src/api/fetchLocations.js
@@ -0,0 +1,7 @@
+export async function fetchLocations() {
+  const res = await fetch("https://rickandmortyapi.com/api/location");
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+  return res.json();
+}
