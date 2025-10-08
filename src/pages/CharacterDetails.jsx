44ae0691915ec25759952c0a6251fd5f842672ee
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-08 20:23:16 +0300

    заглушка и проверка получения данных

--- src/pages/CharacterDetails.jsx
+++ src/pages/CharacterDetails.jsx
@@ -1,3 +1,17 @@
+import { useLocation, useParams } from "react-router-dom";
+
 export default function CharacterDetails() {
-    return <>CharacterDetails</>
+  const { characterId } = useParams();
+  const characters = useLocation();
+
+  const currentCharacter = characters.state.results.find(
+    (character) => Number(character.id) == Number(characterId)
+  );
+
+  return (
+    <div className="container">
+      <img src={currentCharacter.image} alt="" style={{borderRadius:"100%"}}/>
+      {currentCharacter.name}
+    </div>
+  );
 }
