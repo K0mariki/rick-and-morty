100e25b998b8bee8d6a91ee05b8d0675cd643dae
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-08 20:15:39 +0300

    фикс: лого не загружалось на странице CharacterDetails.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
 import "./Logo.scss";
+import logo from "/public/logo.svg"
 
 export default function Logo(props) {
   const { className } = props;
   return (
     <Link
       className={`logo ${className ? className : ""}`}
-      to={"characters"}
+      to="/"
       title="Characters"
       aria-label="Characters"
     >
       <img
         className="logo__image"
-        src="logo.svg"
+        src={logo}
         alt=""
         width="46"
         height="49"
