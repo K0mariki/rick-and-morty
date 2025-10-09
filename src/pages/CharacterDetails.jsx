409e526253346570fef7904b50aaad6c63b6d3a2
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-09 20:30:14 +0300

    Верстка вынесена в компонент SectionDetails

--- src/pages/CharacterDetails.jsx
+++ src/pages/CharacterDetails.jsx
@@ -1,13 +1,38 @@
 import { useLocation } from "react-router-dom";
+import { getEntitysByIds } from "./../api";
+import SectionDetails from "../sections/SectionDetails";
+import { useEffect, useState } from "react";
 
 export default function CharacterDetails() {
-  // const { characterId } = useParams();
-  const character = useLocation();
+  const location = useLocation();
+
+  const [episode, setEpisode] = useState([]);
+
+  useEffect(() => {
+    async function getCast() {
+      const data = await getEntitysByIds("episode", location.state.episode);
+
+      setEpisode(data);
+    }
+    getCast();
+  }, []);
+
+  console.log(location);
 
   return (
-    <div className="container">
-      <img src={character.state.image} alt={character.state.name} style={{borderRadius:"100%"}}/>
-      {character.state.name}
-    </div>
+    <>
+      <SectionDetails
+        image={location.state.image}
+        title={location.state.name}
+        details={[
+          { name: "Gender", value: location.state.gender },
+          { name: "Status", value: location.state.status },
+          { name: "Specie", value: location.state.species },
+          { name: "Origin", value: location.state.origin.name },
+          { name: "Type", value: location.state.type },
+        ]}
+        episode={episode}
+      />
+    </>
   );
 }
