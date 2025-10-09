b4323c1b6c20393312faa33745dc276f7bca23d1
Ermolov Andrey <AwoK.s@mail.ru>
2025-10-09 16:10:26 +0300

    Добавлен вывод принимавших участие персонажей

--- src/pages/EpisodeDetails.jsx
+++ src/pages/EpisodeDetails.jsx
@@ -1,3 +1,47 @@
+import { useLocation } from "react-router-dom";
+import { getEntitysByIds } from "./../api";
+import { useEffect, useState } from "react";
+import CardSection from "./../sections/CardSection";
+import Loader from "../components/Loader/Loader";
+
 export default function EpisodeDetails() {
-    return <>EpisodeDetails</>
+  const location = useLocation();
+  const [casts, setCasts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  console.log(location);
+
+  useEffect(() => {
+    async function getCast() {
+      setIsLoading(true);
+      try {
+        const data = await getEntitysByIds(
+          "character",
+          location.state.characters
+        );
+
+        setCasts(data);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getCast();
+  }, []);
+
+  return (
+    <>
+      {casts.length > 0 || casts["info"] ? (
+        <CardSection
+          sectionsData={casts}
+          linkTo="character"
+          title="Residents"
+        />
+      ) : (
+        ""
+      )}
+      {isLoading && <Loader/>}
+    </>
+  );
 }
--- src/pages/LocationDetails.jsx
+++ src/pages/LocationDetails.jsx
@@ -1,3 +1,44 @@
+import { useLocation } from "react-router-dom";
+import { getEntitysByIds } from "./../api";
+import { useEffect, useState } from "react";
+import CardSection from "./../sections/CardSection";
+import Loader from "../components/Loader/Loader";
+
 export default function LocationDetails() {
-    return <>LocationDetails</>
+  const location = useLocation();
+  const [residents, setResidents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getResidents() {
+      setIsLoading(true);
+      try {
+        const data = await getEntitysByIds(
+          "character",
+          location.state.residents
+        );
+        setResidents(data);
+      } catch (err) {
+        console.log(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getResidents();
+  }, []);
+
+  return (
+    <>
+      {residents.length > 0 || residents["info"] ? (
+        <CardSection
+          sectionsData={residents}
+          linkTo="character"
+          title="Residents"
+        />
+      ) : (
+        ""
+      )}
+      {isLoading && <Loader/>}
+    </>
+  );
 }
